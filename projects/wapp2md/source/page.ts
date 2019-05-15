003e6dbc9e4bf48eb50427ff8dd18e2d09abc690
Aster <galaster@foxmail.com>
2019-05-16 00:08:37 +0800

    🌿 Promise -> Async

--- projects/wapp2md/source/page.ts
+++ projects/wapp2md/source/page.ts
@@ -2,20 +2,15 @@ import * as request from 'request-promise'
 import * as cheerio from 'cheerio'
 import { parserFloor } from './parsers'
 
-export function parserPage(id: number) {
+export async function parserPage(id: number) {
     const options = {
         uri: `http://tieba.baidu.com/mo/m?kz=${id}`,
         transform: function (body: string) {
             return cheerio.load(body, { xmlMode: true })
         }
     }
-    return request(options)
-        .then(($: CheerioStatic) => {
-            const floors = $('body > div > div:nth-child(4)')
-            return floors.children()
-        })
-        .then($ => {
-            const list = $.map(parserFloor).toArray()
-            return list.join('\n\n------------------------------------------\n\n')
-        })
+    const selector = await request(options)
+    const floors = selector('body > div > div:nth-child(4)')
+    const list = floors.children().map(parserFloor).toArray()
+    return list.join('\n\n------------------------------------------\n\n')
 }
