f992fee1ae3c920fde840e1defd17fbd6bc12417
Aster <galaster@foxmail.com>
2019-05-15 22:59:39 +0800

    🌿 Add parser config

---
+++ Readme.md
@@ -0,0 +1,8 @@
+Tieba => Markdown
+=================
+
+保存贴吧的精品文章, 转化为 markdown 格式.
+
+## Tutorial
+
+
---
+++ projects/wapp2md/source/config.ts
@@ -0,0 +1,10 @@
+export class Config {
+    /* Tieba level */
+    // 只要精品
+    // 爬取本吧图库
+
+    /* Tiezi level*/
+    // 主要楼主
+    // 抓取楼中楼, 无此接口
+    // 抓取层主信息
+}
