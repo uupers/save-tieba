86a0314b4bbc7ab6042670d38d0a6089b8774215
Aster <galaster@foxmail.com>
2019-05-15 23:00:44 +0800

    🔧 Rewrite floor heading

--- projects/wapp2md/source/parsers.ts
+++ projects/wapp2md/source/parsers.ts
@@ -5,32 +5,44 @@ function fixLink(src: string) {
     }
 }
 
-function fixText(s: string, i: number) {
-    if (i == 0) {
-        return s.substring(s.indexOf('楼. ') + 3) // 因为是三个字符
-    }
-    else {
-        return s
-    }
+function fixEmoji(src: string) {
+    const img = decodeURIComponent(src)
+    return `![](${img})`
 }
 
+function fixText(s: string) {
+    return s.substring(s.indexOf('楼. ') + 3) // 因为是三个字符
+}
+
+function parserHead(o: CheerioElement) {
+    const s = o.data.indexOf('楼. ')
+    const head = o.data.substring(s + 3)
+    const rest = o.data.substring(0, s)
+    return `## ${head}\n${rest}`
+}
 
 export function parserLine(o: CheerioElement, i: number) {
     switch (o.type) {
-        case 'text': return fixText(o.data, i)
+        case 'text': return o.data
         case 'tag':
             switch (o.name) {
                 case 'br': return '\n\n'
                 case 'a': return fixLink(o.attribs.href)
-                case 'table': return `@unknow(${i + 1}楼 ${o})`
+                case 'img': return fixEmoji(o.attribs.src)
+                case 'table': return `@unknow(${o})`
             }
         default:
             //调试
-            //console.log(o)
+            console.log(o)
             break
     }
 }
 
-export function parserFloor(index: number, o: CheerioElement) {
-    return o.children.map(parserLine).join('')
+export function parserFloor(i: number, o: CheerioElement) {
+    const cells = o.children
+    let element: string[] = [parserHead(cells[0])]
+    for (let x = 1; x < cells.length - 1; x++) {
+        element = element.concat(parserLine(cells[x], x))
+    }
+    return element.join('')
 }
