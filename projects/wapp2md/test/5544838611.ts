2473bff497454f475b2fe7115abcc0d14749c671
Aster <galaster@foxmail.com>
2019-05-15 21:21:32 +0800

    💉 Add test for wapp2md

---
+++ projects/wapp2md/test/5544838611.ts
@@ -0,0 +1,25 @@
+import * as request from 'request-promise';
+import * as cheerio from 'cheerio';
+import { writeFileSync } from "fs";
+
+import { parserFloor } from "../source";
+
+let options = {
+    uri: 'http://tieba.baidu.com/mo/m?kz=5544838611',
+    transform: function (body: string) {
+        return cheerio.load(body, { xmlMode: true })
+    }
+};
+
+
+const markdown = request(options)
+    .then(($: CheerioStatic) => {
+        const floors = $('body > div > div:nth-child(4)');
+        return floors.children()
+    })
+    .then($ => {
+        const list = $.map(parserFloor).toArray()
+        return list.join('\n\n------------------------------------------\n\n')
+    })
+
+markdown.then($ => { writeFileSync(`${__dirname}/5544838611.md`, $) })
