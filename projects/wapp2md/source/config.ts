6b221b0b7ca0aa4dc9a03b8c08bae11e56ae92c7
Morning522 <51274192+Morning522@users.noreply.github.com>
2019-06-02 04:31:16 +0800

    Update config.ts

--- projects/wapp2md/source/config.ts
+++ projects/wapp2md/source/config.ts
@@ -1,10 +1,9 @@
-export class Config {
-    /* Tieba level */
-    // 只要精品
-    // 爬取本吧图库
 
-    /* Tiezi level*/
-    // 主要楼主
-    // 抓取楼中楼, 无此接口
-    // 抓取层主信息
-}
+import { writeFileSync } from 'fs'
+import { parseTiezi } from '../source'
+
+
+
+
+parseTiezi(5544838611)
+    .then($ => { writeFileSync(`${__dirname}/5544838611.md`, $) })
